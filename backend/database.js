52a84ba0728adcaa227f33b17ed25ffaacafc9a6
akshaypx <akshaysrivastava46@gmail.com>
2024-12-15 22:13:17 +0530

    initial commit

---
+++ backend/database.js
@@ -0,0 +1,21 @@
+import { mongoose } from "mongoose";
+import { config } from "dotenv";
+
+config();
+
+const user = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@cluster0.0t9zmus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const connectToDb = async () => {
+  try {
+    const conn = await mongoose.createConnection(uri);
+    conn.on("connected", () => {
+      console.log("Connected to db");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default connectToDb;
---
+++ backend/index.js
@@ -0,0 +1,3 @@
+import connectToDb from "./database.js";
+
+connectToDb();
