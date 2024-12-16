cfdfe4b11b6b5bd5f5ca294594b4b082d10f24a1
akshaypx <akshaysrivastava46@gmail.com>
2024-12-16 21:39:45 +0530

    style: homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      {/* Navbar */}
+      <nav className="px-6 py-4 w-full bg-blue-500 flex justify-center items-center">
+        <div className="flex justify-between max-w-[900px] w-full text-white">
+          <p className="text-xl">Url Shortener</p>
+          <a href="#">Github</a>
+        </div>
+      </nav>
+
+      {/* Body */}
+      <main className="w-full py-6 flex flex-col justify-center items-center h-80">
+        <p className="text-2xl opacity-80 font-bold">
+          Paste your URL to shorten
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form
+          action=""
+          className="flex flex-col md:flex-row max-w-[900px] w-full p-4 items-start justify-center gap-2"
+        >
+          <input
+            type="text"
+            placeholder="https://loooooongwebsite.com/longestever"
+            className="p-2 rounded-md border w-full md:max-w-[80%]"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 w-full rounded-md md:max-w-[20%]"
+          >
+            Shorten!
+          </button>
+        </form>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
